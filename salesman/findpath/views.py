8a1fff912a0a5c3ba08c3730bfa7d02dccd205dd
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2022-02-26 11:05:03 -0500

    Set up test view

--- salesman/findpath/views.py
+++ salesman/findpath/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+@api_view(['POST'])
+def test(request):
+    """
+    {
+        "name": "Bob"
+    }
+    """
+    print(request.data)
+    name = request.data['name']
+    return HttpResponse(f"Hello, {name}.")
