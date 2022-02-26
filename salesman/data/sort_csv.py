043bfc2352109256d21cfebe897a37cca2e62342
Charles Lapierre <charles.lapierre@bell.net>
2022-02-26 11:49:20 -0500

    Update requirements.txt and minimize sort_csv.py

--- salesman/data/sort_csv.py
+++ salesman/data/sort_csv.py
@@ -1,13 +1,7 @@
 import pandas
-import timeit
 
 def sort_csv(file_in: str, file_out: str, categories: list = ["pickup_date_time"], ascending: bool = True):
     sorted = open(file_out, "w")
     csvData = pandas.read_csv(file_in)
     csvData.sort_values(categories, axis=0, ascending=[ascending], inplace=True)
     csvData.to_csv(sorted, index=False, line_terminator='\n')
-    # sorted.write(csv)
-
-start = timeit.default_timer()
-sort_csv("123Loadboard_CodeJam_2022_dataset.csv", "Sorted.csv", ["origin_city"])
-print(timeit.default_timer() - start)
